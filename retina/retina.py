654ac6d844d318e6f7f53212dda0ab96f21a0809
Peter Keffer <8448727+PeterKeffer@users.noreply.github.com>
2024-04-24 13:10:33 +0200

    Update retina.py
    changed pad from skimage to numpy

--- retina/retina.py
+++ retina/retina.py
@@ -1,6 +1,7 @@
 import numpy as np
+from numpy import pad
 from .common import delta_lookup, fit_func
-from skimage.util import crop, pad
+from skimage.util import crop
 from skimage.transform import resize, warp
 
 npa = np.array
